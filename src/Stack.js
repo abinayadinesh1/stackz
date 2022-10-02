841b23ab39f6804f2f1345dbeeab5030dbc31e35
ABINAYA DINESH <abinayadinesh1@gmail.com>
2022-10-01 21:35:57 -0700

    little div container

---
+++ src/Stack.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+// import {stackInfo} from './stackInfo'
+
+const Stack = (stackInfo) => {
+    return(
+        <div className='rectangle'>
+            <h1>el boook</h1>
+        </div>
+    );
+}
+
+export default Stack;
---
+++ src/stackInfo.js
@@ -0,0 +1,17 @@
+export const stackInfo = [{
+  id: 1,
+  category: "Society and Regenerative Design",
+  title: "The Death and Life of Great American Cities",
+  author: "Jane Jacobs"
+}, {
+  id: 2,
+  category: "Science Fiction",
+  title: "Three Body Problem",
+  author: "Cixin Liu"
+},
+{
+  id: 3,
+  category: "Fiction",
+  title: "Atlas Shrugged",
+  author: "Ayn Rand"
+}];
